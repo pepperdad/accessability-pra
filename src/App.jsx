e8de5ca8455a1b0b816ec9290c551ed7baa2fd05
jerry <becomegoodguy@gmail.com>
2024-06-28 20:11:49 +0900

    fix: 이미지만 보이도록

--- src/App.jsx
+++ src/App.jsx
@@ -12,25 +12,33 @@ function App() {
           gap: "20px",
           marginBottom: "30px",
           flexDirection: "column",
+          alignItems: "center",
         }}
       >
-        <img style={{ height: "220px" }} src="/assets/image1.jpeg" />
+        <img
+          style={{ width: "200px", height: "220px" }}
+          src="/assets/image1.jpeg"
+        />
 
         <img
-          style={{ height: "220px" }}
+          style={{ width: "200px", height: "220px" }}
           src="/assets/image2.png"
           alt="귀여운 화랑이"
         />
 
-        <img style={{ height: "220px" }} src="/assets/image3.jpeg" alt="" />
+        <img
+          style={{ width: "200px", height: "220px" }}
+          src="/assets/image3.jpeg"
+          alt=""
+        />
       </div>
 
-      <div style={{ display: "flex", gap: "20px" }}>
+      {/* <div style={{ display: "flex", gap: "20px" }}>
         <button>submit</button>
         <button>제출</button>
         <button aria-label="user info edit">submit</button>
         <button aria-label="유저 정보 수정">submit</button>
-      </div>
+      </div> */}
     </>
   );
 }
